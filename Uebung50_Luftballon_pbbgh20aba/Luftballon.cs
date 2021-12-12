fded1869f1e4f0fbac64e4a8b1e9d9bd271a4358
mdbasoglu <69213997+mdbasoglu@users.noreply.github.com>
2021-12-12 09:32:03 +0100

    erstesCommit

---
+++ Uebung50_Luftballon_pbbgh20aba/Luftballon.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Uebung50_Luftballon_pbbgh20aba
+{
+    class Luftballon
+    {
+
+
+
+        // Attribute
+        private string farbe;
+        private string form;
+        private double maxVolumen;
+        private double volumen;
+        private bool verknotet;
+
+        //Yandaki isarete tikla ve generate Constructor sec. sonrasinda attributlari sec ve bitti!
+        public Luftballon(string farbe, string form, double maxVolumen, double volumen, bool verknotet)
+        {
+            this.farbe = farbe;
+            this.form = form;
+            this.maxVolumen = maxVolumen;
+            this.volumen = volumen;
+            this.verknotet = verknotet;
+        }
+
+        public Luftballon(string farbe, string form, double maxVolumen)
+        {
+            this.farbe = farbe;
+            this.form = form;
+            this.maxVolumen = maxVolumen;
+
+            //Optional
+            volumen = 0;
+            verknotet = false;
+        }
+
+        public Luftballon()
+        {
+            farbe = "rot";
+            form = "rund";
+            maxVolumen = 1.0;
+        }
+
+        //Objekt Methoden
+        public void Aufpusten(double volumen)
+        {
+            if (!verknotet)
+            {
+                this.volumen += volumen;
+                if (this.volumen > maxVolumen)
+                {
+                    Platzen();
+                }
+            }
+        }
+
+        public void verknoten()
+        {
+            verknotet = true;
+        }
+
+        public void Platzen()
+        {
+            Console.WriteLine("WWWWWWWWWWWWWWW............................");
+            volumen = 0;
+            maxVolumen = 0;//bedeuten zerplatzt
+        }
+
+        //Getter-Setter
+        public string GetFarbe()
+        {
+            return farbe;
+        }
+
+        public void SetFarbe(string value)
+        {
+            farbe = value;
+        }
+
+        //Get-Set
+        public string Farbe
+        {
+            get { return farbe; }
+            set { farbe = value; }
+        }
+        //oder public string Farbe{get=>farbe; set=>farbe=value;} wir können mit eine Zeile schreiben
+
+        public override string ToString()
+        {
+            return $"Luftballon [farbe={farbe}, form={form}],maxVolumen={maxVolumen},volumen={volumen}, verknotet={verknotet}";
+        }
+    }
+}
---
+++ Uebung50_Luftballon_pbbgh20aba/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Uebung50_Luftballon_pbbgh20aba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Luftballon ballon; //Deklaration
+            ballon = new Luftballon(); // Initilisierung
+            Luftballon ballon2 = new Luftballon();
+
+            Luftballon blauerLB = new Luftballon("blau", "rund", 1.2);
+            Luftballon gelberLB = new Luftballon("gelb", "längerlich", 0.9, 0.85, true);
+
+
+            blauerLB.Aufpusten(0.3);
+            blauerLB.Aufpusten(0.5);
+            Console.WriteLine(blauerLB);
+
+            blauerLB.verknoten();
+            Console.WriteLine(blauerLB);
+
+            blauerLB.Aufpusten(0.3);
+            Console.WriteLine(blauerLB);
+
+            gelberLB.Platzen();
+            Console.WriteLine(gelberLB);
+
+
+
+            //Getter-Setter
+            string farbe = blauerLB.GetFarbe();
+            blauerLB.SetFarbe("Grünnnnnnnnnnnnnnnn");
+            Console.WriteLine(blauerLB);
+
+            //Get-Set
+            string Farbe = blauerLB.Farbe;
+            blauerLB.Farbe = "grüüünnnnaskdsifmisdmvosmvvmdfpovmdfkmv....";
+            Console.WriteLine(blauerLB);
+
+
+
+
+
+        }
+    }
+}
